7ec459c94325c1fe83f46ff5961efdea4228eeeb
Jeremy Lehner <lehner@uw.edu>
2019-09-02 08:34:31 -0700

    Wrote function to scrape number of skins for each champion

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ Created on Thu Aug 29 13:49:27 2019
 @author: jeremy_lehner
 """
 
-# import necessary functions
+# Import required modules and functions
 import os.path
 from os import path
+import sys
+import requests
+import time
+import random
+import datetime
 import pandas as pd
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import chromedriver_binary
 
+# Import custom functions for this project
 from src.get_league_data import get_champion_names
 from src.get_league_data import get_champion_release_dates
+from src.get_league_data import get_number_of_skins
 
 
-champ_names = get_champion_names()
+champ_names = get_champion_names(scrape=False, save=False)
 
-champ_release_dates = get_champion_release_dates()
+champ_release_dates = get_champion_release_dates(scrape=False, save=False)
+
+num_skins = get_number_of_skins(champ_names, scrape=False, save=False)
--- src/get_league_data.py
+++ src/get_league_data.py
@@ -7,6 +7,9 @@ Created on Thu Aug 29 13:17:50 2019
 """
 
 import pandas as pd
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
 import os.path
 from os import path
 
@@ -41,7 +44,9 @@ def get_champion_names(scrape=True, save=True):
             names.to_csv('./data/champion_names.csv', index=False)
 
     elif path.exists('./data/champion_names.csv'):
-        names = pd.read_csv('./data/champion_names.csv', header=None)
+        names = pd.read_csv('./data/champion_names.csv',
+                            header=None,
+                            squeeze=True)
     else:
         print('champion_names.csv file cannot be found!')
         names = []
@@ -77,7 +82,9 @@ def get_champion_release_dates(scrape=True, save=True):
             dates.to_csv('./data/champion_release_dates.csv', index=False)
 
     elif path.exists('./data/champion_release_dates.csv'):
-        dates = pd.read_csv('./data/champion_release_dates.csv', header=None)
+        dates = pd.read_csv('./data/champion_release_dates.csv',
+                            header=None,
+                            squeeze=True)
     else:
         print('champion_release_dates.csv file cannot be found!')
         dates = []
@@ -85,13 +92,15 @@ def get_champion_release_dates(scrape=True, save=True):
     return dates
 
 
-def get_number_of_skins(scrape=True, save=True):
+def get_number_of_skins(names, scrape=True, save=True):
     """
     Scrapes number of champion skins from League of Legends Wiki,
       returns pandas series of number of skins as integers
 
     Parameters
     ----------
+    names  : pandas series
+             Contains all of the champion names as strings
     scrape : boolean
              Attempt to scrape number of skins for each champion?
     save   : boolean
@@ -104,13 +113,34 @@ def get_number_of_skins(scrape=True, save=True):
     """
 
     if scrape:
-        
+        # Set up selenium web driver
+        driver = webdriver.Chrome()
+
+        # Get number of skins
+        num_skins = []
+        for name in names:
+            name = name.replace(' ', '_')
+            skins_url = f'https://leagueoflegends.fandom.com/wiki/{name}/Skins'
+            driver.get(skins_url)
+            time.sleep(1)
+
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            num_skins.append(len(soup.find_all('div',
+                {'style':'display:inline-block; margin:5px; width:342px'})))
+
+        num_skins = pd.Series(num_skins)
+        driver.close()
+
         if save:
+            num_skins.to_csv('./data/num_skins.csv', index=False)
 
     elif path.exists('./data/num_skins.csv'):
-        num_skins = pd.read_csv('./data/num_skins.csv', header=None)
+        num_skins = pd.read_csv('./data/num_skins.csv',
+                                header=None,
+                                squeeze=True)
     else:
         print('num_skins.csv file cannot be found!')
         num_skins = []
-    
+
     return num_skins
